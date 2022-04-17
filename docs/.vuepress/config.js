7a00ebfb5344fec5742654cbc518e9570fa42de7
Daniel Luca <lucadanielcostin@gmail.com>
2022-04-17 11:41:35 +0200

    🎂 Revert base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     lang: 'en-US',
     title: 'Hello, VuePress!',
     description: 'This is my first VuePress site',
-    base: 'vuepress-test/',
+    base: '/',
 
     // theme and its config
     theme: '@vuepress/theme-default',
