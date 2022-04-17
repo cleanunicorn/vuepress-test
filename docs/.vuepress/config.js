423ec2f5d7ab97af3c6fe11e2f36daa7e72c2843
Daniel Luca <lucadanielcostin@gmail.com>
2022-04-17 12:11:01 +0200

    ㍫

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,5 +9,23 @@ module.exports = {
     theme: '@vuepress/theme-default',
     themeConfig: {
         logo: 'https://vuejs.org/images/logo.png',
+        navbar: [
+            // NavbarItem
+            {
+                text: 'How to',
+                // link: '/how-to/',
+                children: ['/how-to/how-to-read', '/how-to/'],
+            },
+            // NavbarGroup
+            {
+                text: 'Group',
+                children: ['/group/foo.md', '/group/bar.md'],
+            },
+            // string - page file path
+            '/bar/README.md',
+        ],
+        repo: 'cleanunicorn/vuepress-test',
+        repoLabel: 'Code',
+        editLink: false,
     },
 }
---
+++ docs/projects.md
@@ -0,0 +1,7 @@
+Some of my projects
+
+- 1
+- 2
+- 3
+- 4
+- 5
